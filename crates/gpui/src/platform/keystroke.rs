c3fa3984143ae379f2164b09f4ae8a3b6eca78a4
Junkui Zhang <364772080@qq.com>
2025-05-05 20:53:40 +0800

    disblae tests for linux

--- crates/gpui/src/platform/keystroke.rs
+++ crates/gpui/src/platform/keystroke.rs
@@ -579,7 +579,7 @@ impl Modifiers {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Keystroke, Modifiers, TestKeyboardMapper};
+    use crate::Keystroke;
 
     #[test]
     fn test_different_separators() {
@@ -602,7 +602,10 @@ mod tests {
     }
 
     #[test]
+    #[cfg(not(target_os = "linux"))]
     fn test_parse_scan_code() {
+        use crate::{Modifiers, TestKeyboardMapper};
+
         let keyboard_mapper = TestKeyboardMapper::new();
 
         for letter in 'a'..='z' {
